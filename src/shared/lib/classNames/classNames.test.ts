84d2b462af59fdb7431207ed84b6e18e7ed9866a
Aleksey Lovchikov <jwebb@wisebits.com>
2023-05-19 13:55:34 +0300

    add jest setup

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -1,5 +1,33 @@
+import { classNames } from './classNames';
+
 describe('classNames', () => {
-  test('test', () => {
-    expect(true).toBeTruthy();
+  test('with only first param', () => {
+    const expected = 'someClass';
+
+    expect(classNames('someClass')).toBe(expected);
+  });
+
+  test('with additional class', () => {
+    const expected = 'someClass class1 class2';
+
+    expect(classNames('someClass', {}, ['class1', 'class2'])).toBe(expected);
+  });
+
+  test('with mods class', () => {
+    const expected = 'someClass class1 class2 hovered active';
+
+    expect(classNames('someClass', { hovered: true, active: true }, ['class1', 'class2'])).toBe(expected);
+  });
+
+  test('with mods false class', () => {
+    const expected = 'someClass class1 class2 hovered';
+
+    expect(classNames('someClass', { hovered: true, active: false }, ['class1', 'class2'])).toBe(expected);
+  });
+
+  test('with mods undefined class', () => {
+    const expected = 'someClass class1 class2 hovered';
+
+    expect(classNames('someClass', { hovered: true, active: undefined }, ['class1', 'class2'])).toBe(expected);
   });
 });
