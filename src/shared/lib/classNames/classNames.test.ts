554286c78fa7f43e9041d93c3fe12062122a0c46
Aleksey Lovchikov <jwebb@wisebits.com>
2023-05-19 14:00:28 +0300

    eslint fix

--- src/shared/lib/classNames/classNames.test.ts
+++ src/shared/lib/classNames/classNames.test.ts
@@ -16,18 +16,33 @@ describe('classNames', () => {
   test('with mods class', () => {
     const expected = 'someClass class1 class2 hovered active';
 
-    expect(classNames('someClass', { hovered: true, active: true }, ['class1', 'class2'])).toBe(expected);
+    expect(classNames(
+      'someClass',
+      { hovered: true, active: true },
+      ['class1', 'class2'],
+    ))
+      .toBe(expected);
   });
 
   test('with mods false class', () => {
     const expected = 'someClass class1 class2 hovered';
 
-    expect(classNames('someClass', { hovered: true, active: false }, ['class1', 'class2'])).toBe(expected);
+    expect(classNames(
+      'someClass',
+      { hovered: true, active: false },
+      ['class1', 'class2'],
+    ))
+      .toBe(expected);
   });
 
   test('with mods undefined class', () => {
     const expected = 'someClass class1 class2 hovered';
 
-    expect(classNames('someClass', { hovered: true, active: undefined }, ['class1', 'class2'])).toBe(expected);
+    expect(classNames(
+      'someClass',
+      { hovered: true, active: undefined },
+      ['class1', 'class2'],
+    ))
+      .toBe(expected);
   });
 });
