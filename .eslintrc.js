3f28dc63616031fdf5365167b0edf7a2e06c10a8
Aleksey Lovchikov <jwebb@wisebits.com>
2023-05-23 08:33:27 +0300

    fix eslint prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:i18next/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -33,8 +37,14 @@ module.exports = {
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
-    'i18next/no-literal-string': ['error', { markupOnly: true }],
-    'max-len': ['error', { ignoreComments: true }],
+    'i18next/no-literal-string': [
+      'error',
+      { markupOnly: true },
+    ],
+    'max-len': [
+      'error',
+      { ignoreComments: true, code: 120 },
+    ],
   },
   globals: {
     __IS_DEV__: true,
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,15 +2,17 @@ import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExctractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 import { BuildOptions } from './types/config';
 
-export function buildPlugins(
-  options: BuildOptions,
-): webpack.WebpackPluginInstance[] {
+export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstance[] {
   const plugins: webpack.WebpackPluginInstance[] = [
     new webpack.ProgressPlugin(), // показывает прогресс загрузки
-    new HTMLWebpackPlugin({ template: options.paths.html, favicon: '' }), // встраивает скрипты в html
+    new HTMLWebpackPlugin({
+      template: options.paths.html,
+      favicon: options.paths.favicon,
+    }), // встраивает скрипты в html
     new MiniCssExctractPlugin({
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css',
@@ -18,13 +20,11 @@ export function buildPlugins(
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(options.isDev),
     }),
+    new BundleAnalyzerPlugin({ openAnalyzer: false }),
   ];
 
   if (options.isDev) {
-    plugins.push(
-      new webpack.HotModuleReplacementPlugin(),
-      new ReactRefreshWebpackPlugin(),
-    );
+    plugins.push(new webpack.HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin());
   }
 
   return plugins;
