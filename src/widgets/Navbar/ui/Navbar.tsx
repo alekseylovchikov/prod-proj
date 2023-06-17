d9eec13d431f7060191d7cbaee4b59bfd980786a
Aleksey Lovchikov <svdmusic@gmail.com>
2023-06-17 21:03:32 +0300

    fix

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,8 +1,8 @@
+import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 
 import cls from './Navbar.module.scss';
-import { useTranslation } from 'react-i18next';
 
 interface NavbarProps {
   className?: string;
