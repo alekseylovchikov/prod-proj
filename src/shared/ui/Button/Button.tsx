1bc4956a8588c5376d0fd835f91fdfcd498f7011
Aleksey Lovchikov <jwebb@wisebits.com>
2023-07-17 18:28:45 +0300

    fix lint rule

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -23,7 +23,9 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export const Button: FC<ButtonProps> = (props) => {
-  const { className, children, theme, square, size = ButtonSize.M, ...otherProps } = props;
+  const {
+    className, children, theme, square, size = ButtonSize.M, ...otherProps
+  } = props;
 
   const mods: Record<string, boolean> = {
     [cls[theme]]: true,
