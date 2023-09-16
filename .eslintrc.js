dc98453b8f812bb2e5b01c17e1d69eb613dd0bd6
Aleksey Lovchikov <svdmusic@gmail.com>
2023-09-16 18:20:47 +0300

    Disable object-curly-newline

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
     'max-len': ['error', { ignoreComments: true, code: 140 }],
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
+    'object-curly-newline': 'off',
   },
   globals: {
     __IS_DEV__: true,
