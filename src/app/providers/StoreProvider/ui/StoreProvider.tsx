4869c95ae713ec6454033b38538e297bd0af2090
Aleksey Lovchikov <svdmusic@gmail.com>
2023-10-08 13:39:42 +0300

    Configure redux

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,17 +1,17 @@
-import { ReactNode } from "react";
-import { Provider } from "react-redux"
-import { createReduxStore } from "../config/store";
+import { ReactNode } from 'react';
+import { Provider } from 'react-redux';
+import { createReduxStore } from '../config/store';
+import { StateSchema } from '../config/StateSchema';
 
-interface StoreProvider {
-    children: ReactNode;
+interface StoreProviderProps {
+  children?: ReactNode;
+  initialState?: StateSchema;
 }
 
-const store = createReduxStore();
+export const StoreProvider = (props: StoreProviderProps) => {
+  const { children, initialState } = props;
 
-export const StoreProvider = (props: StoreProvider) => {
-    const { children } = props;
+  const store = createReduxStore(initialState);
 
-    return (
-        <Provider store={store}>{children}</Provider>
-    );
+  return <Provider store={store}>{children}</Provider>;
 };
