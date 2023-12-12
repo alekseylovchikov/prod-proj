3f6f43de7445445dbeb7a098eb16b8d0ef257c11
Aleksey Lovchikov <svdmusic@gmail.com>
2023-12-12 15:28:39 -0300

    add json server for fake backend

---
+++ json-server/index.js
@@ -0,0 +1,44 @@
+const jsonServer = require('json-server');
+const path = require('path');
+const fs = require('fs');
+
+const server = jsonServer.create();
+
+const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
+
+server.use(async (req, res, next) => {
+  await new Promise((res) => {
+    setTimeout(res, 800);
+  });
+
+  next();
+});
+
+server.use((req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(403).json({ message: 'Access denied' });
+  }
+
+  next();
+});
+
+server.use(jsonServer.defaults());
+server.use(router);
+
+server.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+  const { users } = db;
+
+  const userFromDb = users.find((user) => user.username === username && user.password === password);
+
+  if (userFromDb) {
+    return res.json(userFromDb);
+  }
+
+  return res.status(403).json({ message: 'Access denied' });
+});
+
+server.listen(8000, () => {
+  console.log('JSON Server is running');
+});
