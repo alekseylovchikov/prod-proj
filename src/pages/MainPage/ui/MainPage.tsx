027803f53363ad24689af1eb11c0ce70d1777933
Aleksey Lovchikov <svdmusic@gmail.com>
2024-03-02 18:33:45 -0300

    chore

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,8 @@
 import { BugButton } from 'app/providers/ErrorBoundary';
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { classNames } from 'shared/lib/classNames/classNames';
-import { Input } from 'shared/ui/Input/Input';
 
-// import { Counter } from 'entities/Counter';
 import cls from './MainPage.module.scss';
 
 interface MainPageProps {
@@ -14,14 +11,12 @@ interface MainPageProps {
 
 const MainPage = ({ className }: MainPageProps) => {
   const { t } = useTranslation('main');
-  const [value, setValue] = useState('');
 
   return (
     <div className={classNames(cls.MainPage, {}, [className])}>
       {/* <Counter /> */}
       <h1>{t('main page')}</h1>
       <BugButton />
-      <Input value={value} onChange={setValue} placeholder="Test input" />
     </div>
   );
 };
