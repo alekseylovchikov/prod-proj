75d19d921a56d8db6d8803d47c3e6bb2e8079b77
Aleksey Lovchikov <svdmusic@gmail.com>
2024-03-02 18:36:13 -0300

    add input stories

---
+++ src/shared/ui/Input/Input.stories.tsx
@@ -0,0 +1,30 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { Input } from './Input';
+
+export default {
+  title: 'shared/Input',
+  component: Input,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  value: 'text',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  value: 'text',
+  theme: 'secondary',
+};
+
+export const Red = Template.bind({});
+Red.args = {
+  value: 'text',
+  theme: 'red',
+};
